600f015f09ccb54cdf06786df1b183b4944e8fdf
vienpham <viendev@outlook.com>
2024-02-29 17:24:34 +0700

    fix: update review edit layout

--- src/taskpane/components/demo/ReviewEditedText.tsx
+++ src/taskpane/components/demo/ReviewEditedText.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { applyChangeSelection, getCommentsSelection, tryCatch } from "../../office-document";
-import { Button, Card, CardFooter, Textarea } from "@fluentui/react-components";
-import { Comment16Regular, Delete16Filled, Save16Regular } from "@fluentui/react-icons";
+import { Button, Card, CardFooter, CardHeader, Textarea, Tooltip } from "@fluentui/react-components";
+import { Comment16Regular, Delete16Regular, Save16Regular } from "@fluentui/react-icons";
 import ReviewComment from "./ReviewComment";
 
 export interface ReviewEditedTextProps {
@@ -28,16 +28,21 @@ const ReviewEditedText = (props: ReviewEditedTextProps) => {
   };
 
   return (
-    <Card appearance="filled-alternative">
+    <Card
+      appearance="filled-alternative"
+      floatingAction={
+        <Tooltip content="Delete changed" relationship="label">
+          <Button icon={<Delete16Regular />} onClick={() => removePhraseText(id)} />
+        </Tooltip>
+      }
+    >
+      <CardHeader title={`Edit ${originalText}`} />
       <Textarea value={originalText} appearance="filled-lighter" resize="vertical" />
       <Textarea value={updatedText} appearance="filled-darker" resize="vertical" />
 
       <CardFooter>
         <Button appearance="primary" icon={<Save16Regular />} onClick={() => applySelectText(updatedText)}>
           Apply
-        </Button>
-        <Button appearance="subtle" icon={<Delete16Filled />} onClick={() => removePhraseText(id)}>
-          Cancel
         </Button>
         {/*<Button appearance="transparent" icon={<Comment16Regular />} onClick={getComments}>*/}
         {/*  Comments*/}
